0fd34f110710f343b3fa70f4ce427999891c55a6
Amalia Claire Fergin <acfergin@msn.com>
2018-02-14 14:36:17 -0800

    add albums to front end pics and titles

--- frontend/albums/albums.js
+++ frontend/albums/albums.js
@@ -4,9 +4,30 @@ axios.get('http://localhost:3000/albums')
   // console.log(albumsData);
 
   albumsData.forEach(album => {
-    let dataElement = document.createElement('div')
-    dataElement.textContent = `Type: ${album.title} ${album.image} ${album.dedicated_to}`
-    document.querySelector('#listOfAlbums').appendChild(dataElement)
-    console.log(album);
+    createAlbum(album)
+
+    // let dataElement = document.createElement('div')
+    // dataElement.textContent = `Type: ${album.title} ${album.image} ${album.dedicated_to}`
+    // document.querySelector('#listOfAlbums').appendChild(dataElement)
+    // console.log(album);
   })
 }).catch(err => console.log(err))
+
+
+function createAlbum (album) {
+  let albumDiv = document.createElement('div')
+  let albumImage = document.createElement('img')
+  let cardBody = document.createElement('div')
+  let cardDedicatedTo = document.createElement('div')
+  albumDiv.classList.add('card')
+  albumImage.classList.add('card-img-top')
+  cardBody.classList.add('card-body')
+  cardDedicatedTo.classList.add('card-title')
+
+  albumImage.src=`${album.image}`
+  cardDedicatedTo.textContent = album.title
+
+  albumDiv.appendChild(albumImage)
+  albumDiv.appendChild(cardDedicatedTo)
+  document.querySelector('#listOfAlbums').appendChild(albumDiv)
+}
